a415165b2065096c8cbe63a7542cac7b33be1b2e
Nana <nana@Nanas-MacBook-Pro.local>
2022-07-18 17:45:44 +0000

    heroku postgresDB connected

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: true
+    // ssl: true,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
